9a0152d60cdc5a656677609453d058894fda6b8a
Jason Alberto <alberto.jason@gmail.com>
2023-02-07 21:03:54 -0500

    add array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 var userNames = ["Miru", "Ada", "Felix," "Tamara"];
 var userAges = [40, 2, 1, 34];
-var confirm = [true, true, false, true];
+var truthy = [true, true, false, true];
+
+//remove the element in the index position 3 reducing the elements in the array to 3
+userNames.pop();
+//remove the element in the index position 0 reducing the elements in the array to 3
+userAges.shift();
+//add the boolean false value to the front of the array increasing the number of elements to 5
+truthy.unshift(false);
